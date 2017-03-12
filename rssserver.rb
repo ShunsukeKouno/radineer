1f7b9f85c69c40182ee0d347f396f1b15ea68347
ShunsukeKouno <a5814036@aoyama.jp>
2017-03-12 23:54:32 +0900

    rssserver2

--- rssserver.rb
+++ rssserver.rb
@@ -1,56 +1,157 @@
 require 'open-uri'
 require 'cgi'
 require 'rss'
+require 'kconv'
+require 'webrick'
 
-def parse(page_source)
-  dates = page_source.scan(
-    %r!(\d+)年 ?(\d+)月 ?(\d+)日<br />!)
-  url_titles = page_source.scan(
-    %r!^<a href="(.+?)">(.+?)</a><br />!)
-  url_titles.zip(dates).map{|(aurl,atitle),
-    ymd|[CGI.unescapeHTML(aurl),CGI.unescapeHTML(atitle),
-    Time.local(*ymd)]
-  }
-end
-
-def format_text(title, url ,url_title_time_ary)
-  s = "Title: #{title}\nURL: #{url}\n\n"
-  url_title_time_ary.each do |aurl, atitle, atime|
-  s << "*(#{atime})#{atitle}\n"
-  s << "  #{aurl}\n"
+#rssserver2
+class Site
+  def initialize(url:"", title:"")
+    @url, @title = url ,title
   end
-  s
+  attr_reader :url,:title
+
+  def page_source
+    @page_source ||= open(@url,&:read).toutf8
+  end
+
+  def output(formatter_klass)
+    formatter_klass.new(self).format(parse)
+  end
 end
 
-def format_rss(title, url ,url_title_time_ary)
-  RSS::Maker.make("2.0") do |maker|
-    maker.channel.updated = Time.now.to_s
-    maker.channel.link = url
-    maker.channel.title = title
-    maker.channel.description = title
+class SbcrTopics < Site
+  def parse(page_source)
+    dates = page_source.scan(
+      %r!(\d+)年 ?(\d+)月 ?(\d+)日<br />!)
+    url_titles = page_source.scan(
+      %r!^<a href="(.+?)">(.+?)</a><br />!)
+    url_titles.zip(dates).map{|(aurl,atitle),
+      ymd|[CGI.unescapeHTML(aurl),CGI.unescapeHTML(atitle),
+      Time.local(*ymd)]
+    }
+  end
+end
+
+class Formatter
+  def initialize(site)
+    @url = site.url
+    @title = site.title
+  end
+  attr_reader :url,:title
+end
+
+class TextFormatter < Formatter
+  def format(url_title_time_ary)
+    s = "Title: #{title}\nURL: #{url}\n\n"
     url_title_time_ary.each do |aurl, atitle, atime|
-      maker.items.new_item do |item|
-        item.link = aurl
-        item.title = atitle
-        item.updated = atime
-        item.description = atitle
+      s << "*(#{atime})#{atitle}\n"
+      s << "  #{aurl}\n"
+    end
+    s
+  end
+end
+
+class RSSFormatter < Formatter
+  def format(url_title_time_ary)
+    RSS::Maker.make("2.0") do |maker|
+      maker.channel.updated = Time.now.to_s
+      maker.channel.link = url
+      maker.channel.title = title
+      maker.channel.description = title
+      url_title_time_ary.each do |aurl, atitle, atime|
+        maker.items.new_item do |item|
+          item.link = aurl
+          item.title = atitle
+          item.updated = atime
+          item.description = atitle
+        end
       end
     end
   end
 end
 
-parsed = parse(open(
-  "http://crawler.sbcr.jp/samplepage.html",
-  "r:UTF-8",&:read))
+class RSSServlet <
+  WEBrick::HTTPServlet::AbstractServlet
 
-puts ARGV.first
-formatter = case ARGV.first
-  when "rss-output"
-      :format_rss
-  when "text-output"
-      :format_text
+  def do_GET(req,res)
+    klass, opts = @options
+    res.body =
+      klass.new(opts).output(RSSFormatter).to_s
+    res.content_type =
+      "application/xml; charset=UTF-8"
+    end
   end
 
-puts __send__(formatter,
-  "WWW.SBCR.JP トピックス",
-  "http://crawler.sbcr.jp/samplepage.html",parsed)
+  def start_server
+    srv = WEBrick::HTTPServer.new(:BindAddress =>
+      '127.0.0.1', :Port => 7777)
+    # srv.mount('/rss.xml',RSSServlet,SbcrTopics,
+    # url:"http://crawler.sbcr.jp/samplepage.html",
+    # title:"WWW.SBCR.JP トピックス")
+    #場合によってはsrv.mount行を追加する
+    trap("INT"){srv.shutdown}
+    srv.start
+  end
+
+  if ARGV.first == 'server'
+    start_server
+  else
+    site =  SbcrTopics.new(
+      url:"http://crawler.sbcr.jp/samplepage.html",
+      title:"WWW.SBCR.JP トピックス")
+
+    case ARGV.first
+      when "rss-output"
+          puts site.output RSSFormatter
+      when "text-output"
+          puts site.output TextFormatter
+    end
+  end
+
+
+
+
+#rssserver0
+
+# def format_rss(title, url ,url_title_time_ary)
+#   RSS::Maker.make("2.0") do |maker|
+#     maker.channel.updated = Time.now.to_s
+#     maker.channel.link = url
+#     maker.channel.title = title
+#     maker.channel.description = title
+#     url_title_time_ary.each do |aurl, atitle, atime|
+#       maker.items.new_item do |item|
+#         item.link = aurl
+#         item.title = atitle
+#         item.updated = atime
+#         item.description = atitle
+#       end
+#     end
+#   end
+# end
+
+# def format_text(title, url ,url_title_time_ary)
+#   s = "Title: #{title}\nURL: #{url}\n\n"
+#   url_title_time_ary.each do |aurl, atitle, atime|
+#   s << "*(#{atime})#{atitle}\n"
+#   s << "  #{aurl}\n"
+#   end
+#   s
+# end
+
+# parsed = parse(open(
+#   "http://crawler.sbcr.jp/samplepage.html",
+#   "r:UTF-8",&:read))
+#
+# puts ARGV.first
+# formatter = case ARGV.first
+#   when "rss-output"
+#       :format_rss
+#   when "text-output"
+#       :format_text
+#   end
+#
+# puts __send__(formatter,
+#   "WWW.SBCR.JP トピックス",
+#   "http://crawler.sbcr.jp/samplepage.html",parsed)
